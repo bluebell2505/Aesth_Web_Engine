a38e578eee60d878884dea013d7d154e7f22de9d
Dhruv Rajhans <149691783+CoderDhruv23@users.noreply.github.com>
2025-12-10 12:59:29 +0530

    Revert "Model for OCR version 1"

--- tests/test_pipeline.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-# if running locally:
-url = "http://localhost:8000/api/extract-aesthetic"
-files = {'file': open("tests/sample1.jpg","rb")}
-r = requests.post(url, files=files, timeout=20)
-print(r.status_code, r.json())
